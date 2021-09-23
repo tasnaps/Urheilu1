e92cbc4b1164a399eaaa5b264fb431f222dda539
tasnaps <74553493+tasnaps@users.noreply.github.com>
2021-09-23 12:10:12 +0300

    Initialization

---
+++ urheilu1/urheilu1.js
@@ -0,0 +1,16 @@
+class Henkilo{
+    constructor(etunimet, sukunimi, kutsumanimi, syntymavuosi) {
+        this.etunimet = etunimet;
+        this.sukunimi = sukunimi;
+        this.kutsumanimi = kutsumanimi;
+        this.syntymavuosi = syntymavuosi;
+    }
+}
+
+class urheilija extends Henkilo{
+    constructor(etunimet, sukunimi, kutsumanimi, syntymavuosi) {
+        super();
+    }
+
+
+}
